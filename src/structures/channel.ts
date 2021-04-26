c9c30b52817059e9a93e0f621237c0e6ed23a67a
zhaoYangguang <1163765691@qq.com>
2021-04-26 19:04:11 +0800

    first commit

---
+++ src/structures/channel.ts
@@ -0,0 +1,299 @@
+/*
+Copyright 2020 soru-slack-client
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Client } from "./client";
+import { Base, IIconData, ICreatorValue } from "./base";
+import { Team } from "./team";
+import { User } from "./user";
+import { Util } from "../util";
+
+type ChannelTypes = "channel" | "group" | "mpim" | "im" | "unknown";
+
+export interface IChannelData {
+	id: string;
+	name?: string;
+	is_channel: boolean;
+	is_group: boolean;
+	is_mpim: boolean;
+	is_im: boolean;
+	is_archived?: boolean;
+	topic?: ICreatorValue;
+	purpose?: ICreatorValue;
+	private?: boolean;
+	is_private?: boolean;
+	is_open?: boolean;
+	team_id?: string;
+	shared_team_ids?: string[];
+	user?: string;
+}
+
+interface ISendMessage {
+	text: string;
+	blocks?: any[]; // tslint:disable-line no-any
+}
+
+type SendableType = ISendMessage | string;
+
+export interface ISendOpts {
+	username?: string | null;
+	iconUrl?: string | null;
+	iconEmoji?: string | null;
+	threadTs?: string | null;
+	asUser?: boolean | null;
+}
+
+export class Channel extends Base {
+	public members: Map<string, User> = new Map();
+	public id: string;
+	public name: string | null = null;
+	public type: ChannelTypes;
+	public topic: string | null = null;
+	public purpose: string | null = null;
+	public private: boolean;
+	public team: Team;
+	public partial = true;
+	private joined = false;
+	private isArchived = false;
+	private isOpen = false;
+	constructor(client: Client, data: IChannelData) {
+		super(client);
+		const teamId = data.team_id || (data.shared_team_ids && data.shared_team_ids[0]);
+		if (!teamId) {
+			throw new Error("no associated team!");
+		}
+		const team = this.client.teams.get(teamId);
+		if (!team) {
+			throw new Error("team not found!");
+		}
+		this.team = team;
+		this._patch(data);
+	}
+
+	public get fullId(): string {
+		return `${this.team.id}${this.client.separator}${this.id}`;
+	}
+
+	public _patch(data: IChannelData) {
+		this.id = data.id;
+		this.name = data.name || null;
+		let type: ChannelTypes = "unknown";
+		if (data.is_channel) {
+			type = "channel";
+		} else if (data.is_mpim) {
+			type = "mpim";
+		} else if (data.is_group) {
+			type = "group";
+		} else if (data.is_im) {
+			type = "im";
+		}
+		this.type = type;
+		if (data.hasOwnProperty("topic")) {
+			this.topic = data.topic!.value;
+		}
+		if (data.hasOwnProperty("purpose")) {
+			this.purpose = data.purpose!.value;
+		}
+		this.private = Boolean(data.is_im || data.private || data.is_private);
+		if (data.is_archived !== undefined && data.is_archived !== null) {
+			this.isArchived = data.is_archived;
+		}
+		if (data.is_open !== undefined && data.is_open !== null) {
+			this.isOpen = data.is_open;
+		}
+		if (data.hasOwnProperty("user")) {
+			const userObj = this.team.users.get(data.user!);
+			if (userObj) {
+				this.members.set(userObj.id, userObj);
+			}
+		}
+	}
+
+	public async load() {
+		// first load the info
+		{
+			const ret = await this.client.web(this.team.id).conversations.info({
+				channel: this.id,
+			});
+			if (!ret || !ret.ok || !ret.channel) {
+				throw new Error("Bad response");
+			}
+			this._patch(ret.channel as IChannelData);
+		}
+		// now load the members
+		{
+			let cursor: string | undefined;
+			do {
+				const ret = await this.client.web(this.team.id).conversations.members({
+					channel: this.id,
+					limit: 1000,
+					cursor,
+				});
+				if (!ret || !ret.ok || !ret.members) {
+					throw new Error("Bad response");
+				}
+				for (const memberId of ret.members as string[]) {
+					const userObj = this.team.users.get(memberId);
+					if (userObj) {
+						this.members.set(userObj.id, userObj);
+					}
+				}
+				cursor = ret.response_metadata && ret.response_metadata.next_cursor;
+			} while (cursor);
+		}
+		this.partial = false;
+	}
+
+	public async join() {
+		if (["im", "mpim", "group"].includes(this.type) || this.private || this.joined) {
+			return;
+		}
+		await this.client.web(this.team.id).conversations.join({
+			channel: this.id,
+		});
+		this.joined = true;
+	}
+
+	public async sendTyping() {
+		await this.client.rtm(this.team.id).sendTyping(this.id);
+	}
+
+	public async sendMessage(sendable: SendableType, opts?: ISendOpts): Promise<string> {
+		await this.join();
+		const send: any = { // tslint:disable-line no-any
+			...this.resolveSendable(sendable),
+			channel: this.id,
+		};
+		this.applyOpts(send, opts);
+		const ret = await this.client.web(this.team.id).chat.postMessage(send);
+		return ret.ts as string;
+	}
+
+	public async sendCommand(command: string, parameters?: string): Promise<string> {
+		if (this.team.isBotToken()) {
+			throw new Error("Not available with bot tokens");
+		}
+		await this.join();
+		const send: any = { // tslint:disable-line no-any
+			command,
+			text: parameters,
+			channel: this.id,
+		};
+		const ret = await this.client.web(this.team.id).apiCall("chat.command", send);
+		return ret.ts as string;
+	}
+
+	public async sendMeMessage(sendable: SendableType): Promise<string> {
+		await this.join();
+		const send = this.resolveSendable(sendable);
+		if (this.team.isBotToken()) {
+			send.text = `_${send.text}_`;
+			return await this.sendMessage(send);
+		}
+		const ret = await this.client.web(this.team.id).chat.meMessage({
+			...send,
+			channel: this.id,
+		});
+		return ret.ts as string;
+	}
+
+	public async deleteMessage(ts: string, opts?: ISendOpts) {
+		await this.join();
+		const send: any = { // tslint:disable-line no-any
+			channel: this.id,
+			ts,
+		};
+		this.applyOpts(send, opts);
+		await this.client.web(this.team.id).chat.delete(send);
+	}
+
+	public async editMessage(sendable: SendableType, ts: string, opts?: ISendOpts): Promise<string> {
+		await this.join();
+		const send: any = { // tslint:disable-line no-any
+			...this.resolveSendable(sendable),
+			channel: this.id,
+			ts,
+		};
+		this.applyOpts(send, opts);
+		const ret = await this.client.web(this.team.id).chat.update(send);
+		return ret.ts as string;
+	}
+
+	public async sendFile(urlOrBuffer: string | Buffer, title: string, filename?: string): Promise<string> {
+		await this.join();
+		if (!filename) {
+			filename = title;
+		}
+		let buffer: Buffer;
+		if (typeof urlOrBuffer === "string") {
+			buffer = await Util.DownloadFile(urlOrBuffer);
+		} else {
+			buffer = urlOrBuffer;
+		}
+		const ret = await this.client.web(this.team.id).files.upload({
+			filename,
+			file: buffer,
+			title,
+			filetype: "auto",
+			channels: this.id,
+		});
+		return ret.ts as string;
+	}
+
+	public async sendReaction(ts: string, reaction: string) {
+		await this.join();
+		await this.client.web(this.team.id).reactions.add({
+			channel: this.id,
+			timestamp: ts,
+			name: reaction,
+		});
+	}
+
+	public async removeReaction(ts: string, reaction: string) {
+		await this.join();
+		await this.client.web(this.team.id).reactions.remove({
+			channel: this.id,
+			timestamp: ts,
+			name: reaction,
+		});
+	}
+
+	private resolveSendable(sendable: SendableType): ISendMessage {
+		const msg: ISendMessage = typeof sendable === "string" ? {
+			text: sendable,
+		} : sendable;
+		if (this.team.isBotToken() && msg.blocks && msg.blocks[0] && msg.blocks[0].type === "rich_text") {
+			delete msg.blocks;
+		}
+		return msg;
+	}
+
+	private applyOpts(send: any, opts?: ISendOpts) { // tslint:disable-line no-any
+		if (opts) {
+			if (opts.username) {
+				send.username = opts.username;
+				if (opts.iconUrl) {
+					send.icon_url = opts.iconUrl;
+				}
+				if (opts.iconEmoji) {
+					send.icon_emoji = opts.iconEmoji;
+				}
+			}
+			if (opts.asUser) {
+				send.as_user = true;
+			}
+			if (opts.threadTs) {
+				send.thread_ts = opts.threadTs;
+			}
+		}
+	}
+}
---
+++ src/structures/team.ts
@@ -0,0 +1,194 @@
+/*
+Copyright 2020 soru-slack-client
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Client } from "./client";
+import {IconBase, ICreatorValue, IIconData} from "./base";
+import { IChannelData, Channel } from "./channel";
+import { IUserData, User } from "./user";
+import { Bot } from "./bot";
+import {WebAPICallResult} from "@slack/web-api";
+
+export interface ITeamData {
+	id: string;
+	fakeId?: string;
+	name?: string;
+	domain?: string;
+	icon?: IIconData;
+}
+
+export class Team extends IconBase {
+	public channels: Map<string, Channel> = new Map();
+	public users: Map<string, User> = new Map();
+	public bots: Map<string, Bot> = new Map();
+	public id: string;
+	public name: string;
+	public domain: string;
+	public email: string | null = null;
+	public emailDomain: string | null = null;
+	public icon: IIconData | null = null;
+	public enterpriseId: string | null = null;
+	public enterpriseName: string | null = null;
+	public partial = true;
+	public fakeId: string | null = null;
+	constructor(client: Client, data: ITeamData) {
+		super(client);
+		this._patch(data);
+	}
+
+	public _patch(data: ITeamData) {
+		this.id = data.id;
+		if (data.hasOwnProperty("name")) {
+			this.name = data.name!;
+		}
+		if (data.hasOwnProperty("domain")) {
+			this.domain = data.domain!;
+		}
+		if (data.hasOwnProperty("icon")) {
+			this.icon = data.icon || null;
+		}
+		if (data.hasOwnProperty("fakeId")) {
+			this.fakeId = data.fakeId!;
+		}
+		if (this.client.tokens.has(this.id)) {
+			if (this.fakeId && !this.partial) {
+				this.partial = true;
+			}
+			this.fakeId = null;
+		}
+	}
+
+	public async joinAllChannels() {
+		if (this.partial) {
+			await this.load();
+		}
+		for (const [, channel] of this.channels) {
+			await channel.join();
+		}
+	}
+
+	public async load() {
+		// first load the team itself
+		{
+			const ret = await this.client.web(this.fakeId || this.id).team.info({
+				team: this.id,
+			});
+			if (!ret || !ret.ok || !ret.team) {
+				throw new Error("Bad response");
+			}
+			this._patch(ret.team as ITeamData);
+		}
+		if (this.fakeId) {
+			this.partial = false;
+			return;
+		}
+		// next load in the channels
+		{
+			let cursor: string | undefined;
+			do {
+				const ret = await this.client.web(this.id).conversations.list({
+					types: "public_channel,private_channel,mpim,im",
+					limit: 1000,
+					cursor,
+					exclude_archived: true
+				});
+				if (!ret || !ret.ok || !ret.channels) {
+					throw new Error("Bad response");
+				}
+				for (const channelData of ret.channels as IChannelData[]) {
+					channelData.team_id = this.id;
+					this.client.addChannel(channelData);
+				}
+				cursor = ret.response_metadata && ret.response_metadata.next_cursor;
+			} while (cursor);
+		}
+		// next load in the users
+		{
+			let cursor: string | undefined;
+			do {
+				const ret = await this.client.web(this.id).users.list({
+					limit: 1000,
+					cursor,
+				});
+				if (!ret || !ret.ok || !ret.members) {
+					throw new Error("Bad response");
+				}
+				for (const userData of ret.members as IUserData[]) {
+					this.client.addUser(userData);
+				}
+				cursor = ret.response_metadata && ret.response_metadata.next_cursor;
+			} while (cursor);
+		}
+		this.partial = false;
+	}
+
+	public isBotToken(): boolean {
+		const token = this.client.tokens.get(this.id) || "";
+		return token.startsWith("xoxb");
+	}
+
+
+	public async create(name: string, isPrivate:boolean): Promise<string> {
+		const result =  await this.client.web(this.id).conversations.create({
+			name: name,
+			is_private: isPrivate,
+			team_id: this.id
+		});
+		if (result.channel) {
+			const channelData: IChannelData = {
+				id: (<IChannelData>result.channel).id,
+				name: name,
+				is_channel: (<IChannelData>result.channel).is_channel,
+				is_group: (<IChannelData>result.channel).is_group,
+				is_mpim: (<IChannelData>result.channel).is_mpim,
+				is_im: (<IChannelData>result.channel).is_im,
+				is_private: (<IChannelData>result.channel).is_private,
+				team_id: this.id,
+			}
+			this.client.addChannel(<IChannelData>channelData);
+			return (<IChannelData>result.channel).id;
+		} else {
+			return '';
+		}
+		
+	}
+
+	/**
+	 * Invites users to a channel.
+	 * @param channel {string} Required. The ID of the public or private channel to invite user(s) to.
+	 * Example C1234567890
+	 * @param users {string} Required. A comma separated list of user IDs. Up to 1000 users may be listed.
+	 * Example W1234567890,U2345678901,U3456789012
+	 */
+	public async invite(channel: string, users:string): Promise<any> {
+		const result = await this.client.web(this.id).conversations.invite({
+			channel: channel,
+			users: users,
+		});
+		return result.ok;
+	}
+
+	/**
+	 * kick user from a channel.
+	 * @param channel {string} Required. ID of conversation to remove user from.
+	 * Example C1234567890
+	 * @param user {string} Required. User ID to be removed..
+	 * Example W1234567890
+	 */
+	public async kick(channel: string, user: string): Promise<any> {
+		const result = await this.client.web(this.id).conversations.kick({
+			channel: channel,
+			user: user,
+		});
+		return result.ok;
+	}
+}
